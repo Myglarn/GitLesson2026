b0a2d5af7429b75d8071a68d95e13535cb261b15
Myglarn <christopher.petti@gmail.com>
2026-01-08 11:35:54 +0100

    Init commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hej och välkommna till Gitlektionen!");
+Console.WriteLine("Vill du lära dig mer? (J/N)");
+var svar = Console.ReadLine().ToLower();
+
+if (svar == "j")
+{
+    Console.WriteLine("Nu har du lärt dig mer! :) ");
+}
+else if (svar == "n")
+{
+    Console.WriteLine("Nehe, strunt i det då!");
+}
